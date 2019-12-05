698190bc4b841951ed31bff76ef011aacf3f6e0e
Artem Evseenkov <evsweb@yandex.ru>
2019-12-05 18:29:36 +0500

    header layouts + font

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,30 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+@font-face {
+  font-family: main-font; /* Гарнитура шрифта */
+  src: url(assets/circe-rounded.otf); /* Путь к файлу со шрифтом */
+}
+body {
+  font-family: main-font;
+  background-image: url(./assets/temp-img.jpg);
+}
+body::after {
+  content: '';
+  width: 100%;
+  height: 100px;
+  filter: blur(10px);
+}
+a {
+  text-decoration: none;
+  color: #000000;
+}
 .wrapper {
-  max-width: 1000px;
+  max-width: 1920px;
   margin: 0 auto;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,14 @@
 <template>
-<div>
-  <router-link to="/">Home</router-link>
-  <router-link to="/contacts">Contacts</router-link>
+<div class="header">
+  <div class="header_blur">
+    <div class="header_logo"></div>
+    <div class="header_nav">
+      <router-link class="link" to="/">Home</router-link>
+      <router-link class="link" to="/contacts">Contacts</router-link>
+      <router-link class="link" to="/contacts">Привет писикоша</router-link>
+    </div>
+    <div class="header_burger"></div>
+  </div>
 </div>
 </template>
 
@@ -11,4 +18,27 @@ export default {
 </script>
 
 <style scoped>
+.header {
+  width: 100%;
+  max-width: 1920px;
+}
+.header_blur {
+  display: flex;
+  min-height: 100px;
+}
+.header_logo {
+  width: 7%;
+  background: red;
+}
+.header_nav {
+  width: 88%;
+}
+.header_burger {
+  width: 5%;
+  background: blue;
+}
+.link {
+  font-size: 30px;
+  margin-left: 20px;
+}
 </style>
