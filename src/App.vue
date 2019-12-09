0619115d811fcd443bdb5815afc15b6199d9f53c
Artem Evseenkov <evsweb@yandex.ru>
2019-12-09 19:01:06 +0500

    re-disign all... again...

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <div class="wrapper">
-      <Header />
+      <div class="menu_side">
+        <Menu />
+      </div>
+      <div class="content_side">
+        <div class="test_block"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue';
+import Menu from '@/components/Menu.vue';
 
 export default {
   components: {
-    Header,
+    Menu,
   },
 };
 </script>
@@ -22,20 +27,54 @@ export default {
   margin: 0;
 }
 @font-face {
-  font-family: main-font; /* Гарнитура шрифта */
-  src: url(assets/camper.woff); /* Путь к файлу со шрифтом */
+  font-family: regular;
+  src: url(assets/fonts/URWGeometric-Regular.woff);
+}
+@font-face {
+  font-family: fat;
+  src: url(assets/fonts/URWGeometric-Bold.woff);
+}
+@font-face {
+  font-family: megafat;
+  src: url(assets/fonts/URWGeometric-Black.woff);
 }
 body {
-  font-family: main-font;
-  background-color: #000000;
+  font-family: regular;
+  background-color: #FEEDD3;
   color: #24C020;
+  box-sizing: border-box;
 }
 a {
   text-decoration: none;
-  color: #24C020;
+  color: #009900;
 }
 .wrapper {
-  max-width: 1920px;
-  margin: 0 auto;
+  max-width: 1880px;
+  margin: 8px auto;
+  background: #209F84;
+  min-width: 95%;
+  min-height: 800px;
+  border-radius: 40px;
+  display: flex;
+  padding: 7px;
+  box-sizing: border-box;
+}
+.menu_side {
+  width: 18%;
+}
+.content_side {
+  background: #EFFAFC;
+  width: 100%;
+  height: 800px;
+  display: block;
+  border-radius: 40px;
+  padding: 20px;
+  box-sizing: border-box;
+}
+.test_block {
+  width: 90%;
+  height: 200px;
+  background: #ffffff;
+  border-radius: 40px;
 }
 </style>
